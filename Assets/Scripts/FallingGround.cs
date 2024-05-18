96addeeb58669f682f0c45626c34c1445500bb45
Filipe F <111152966+Filipe-Francisco-07@users.noreply.github.com>
2024-05-18 18:06:48 -0300

    com

---
+++ Assets/Scripts/FallingGround.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingGround : MonoBehaviour
+{
+    private TargetJoint2D tj;
+    private BoxCollider2D bc;
+    void Start()
+    {
+        tj = GetComponent<TargetJoint2D>();
+        bc = GetComponent<BoxCollider2D>();
+    }
+
+   void OnCollisionEnter2D(Collision2D collision){
+        if(collision.gameObject.tag == "Player")
+        {
+            Invoke("Cair",1.5f);
+        }
+       
+    }
+    void Cair(){
+        tj.enabled = false;
+        bc.isTrigger = true;
+    }
+
+}
